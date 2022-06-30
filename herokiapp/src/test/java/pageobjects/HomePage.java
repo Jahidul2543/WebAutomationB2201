d127b29234f57cb32d2b4651e759f509db0fabdc
jahidul2543 <jahidul2543@gmail.com>
2022-06-29 21:17:09 -0500

    First Commit

---
+++ herokiapp/src/test/java/pageobjects/HomePage.java
@@ -0,0 +1,22 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
+
+public class HomePage  {
+    WebDriver driver = null;
+    @FindBy(how = How.LINK_TEXT, using = "File Upload")
+    WebElement fileUploadLink;
+
+
+    public void clickFileUploadLink(){
+        fileUploadLink.click();
+        // String uploadPageTitle = driver.getTitle();
+        // TestLogger.log("Upload Page Title: " + uploadPageTitle);
+    }
+
+}
